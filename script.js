9f6933176d41b9389e25cd3daf5690172d8f3215
unknown <a.alzahabi@ejad.solutions>
2023-07-18 11:28:33 +0300

    finale {{ CRUD  }} project

--- script.js
+++ script.js
@@ -1,31 +1,88 @@
-let task = document.querySelector(".task");
-let title = document.querySelector(".title");
-let date = document.querySelector(".date");
 let tasks = [
   {
-    taskOne: {
-      title: "reade book",
-      date: "20/4/0240",
-    },
-    taskTwo: {
-      title: "reade book2",
-      date: "20/4/0240",
-    },
-    taskThree: {
-      title: "reade book3",
-      date: "20/4/0240",
-    },
+    title: "reade book",
+    date: "20/4/0240",
+    isDone: true,
+  },
+  {
+    title: "reade book2",
+    date: "20/4/0240",
+    isDone: false,
+  },
+  {
+    title: "reade book3",
+    date: "20/4/0240",
+    isDone: true,
   },
 ];
+// CRUD = {{ R }}
 
-console.log(task);
-console.log(tasks[0].taskOne.title);
-console.log(title);
-console.log(date);
+function fetchData() {
+  let index = 0;
 
-tasks.map((el) => {
-  title.innerHTML += `${tasks[0].el.title}`;
-  date.innerHTML += `${tasks[0].el.date}`;
+  document.querySelector(".tasks").innerHTML = "";
+  for (task of tasks) {
+    document.querySelector(".tasks").innerHTML += `
+    <div class="task ${
+      task.isDone ? "done" : ""
+    }" style="display:flex; align-items:center;justify-content:space-between;margin:6px ">
+      <div class="text">
+        <h3 class="title">${task.title}</h3>
+        <p class="date">${task.date}</p>
+      </div>
+      <div style="margin-left:250px">
+        <i onClick="deleteTask(${index})" style="margin-left:15px" class="ri-delete-bin-5-fill delete"></i>
+        ${
+          !task.isDone
+            ? `<i onClick='toggle(${index})' style="margin-left:15px" class="ri-check-double-line check"></i>`
+            : `<i onClick='toggle(${index})' style="margin-left:15px" class="ri-close-circle-fill cancel"></i>`
+        }
+        <i onClick="updateTask(${index})"style="margin-left:15px" class="ri-pencil-fill put"></i>
+      </div>
+    </div>
+    `;
+    index++;
+  }
+}
+fetchData();
 
-  console.log(el);
+// CRUD = {{ C }}
+document.querySelector(".button").addEventListener("click", function () {
+  let newTitle = prompt("ادخل مهمة جديدة");
+  let now = new Date();
+  let date = now.getDate() + "/" + now.getMonth() + "/" + now.getFullYear();
+  let newObject = {
+    title: newTitle,
+    date: date,
+    isDone: false,
+  };
+  tasks.push(newObject);
+  fetchData();
+  console.log(newTitle);
+  console.log(newObject);
 });
+function toggle(index) {
+  task = tasks[index];
+  task.isDone = !task.isDone;
+  fetchData();
+}
+
+// CRUD = {{ D }}
+
+function deleteTask(index) {
+  let conf = confirm("Are you sure you want to delete");
+  if (conf) {
+    tasks.splice(index, 1);
+  }
+
+  fetchData();
+}
+
+// CRUD = {{ U }}
+function updateTask(index) {
+  let NewTask = prompt("مهمة جديدة");
+  let task = tasks[index];
+  console.log(task);
+  task.title = NewTask;
+  fetchData();
+}
